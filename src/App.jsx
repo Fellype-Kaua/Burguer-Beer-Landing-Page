b3e734c40b893bba2209c5bf8127b978849d36b5
Fellype-Kaua <fellypekaua.p.s@gmail.com>
2024-07-08 14:10:49 -0300

    fix: corrige problema de espaço em branco em dispositivos móveis com max-width

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,27 @@
-import React from 'react';
-import './App.scss'
+import React from "react";
+import "./App.scss";
 
-
-
-import Header from './components/Header';
-import WelcomeSection from './components/WelcomeSection';
-import Menu from './components/Menu';
-import AboutUs from './components/AboutUs';
-import ContactUs from './components/ContactUs';
-import Footer from './components/Footer';
-import Testimonial from './components/Testimonials';
+import Header from "./components/Header";
+import WelcomeSection from "./components/WelcomeSection";
+import Menu from "./components/Menu";
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
+import Footer from "./components/Footer";
+import Testimonial from "./components/Testimonials";
 
 const App = () => {
   return (
-    <div>
-      
+    <div id="App">
       <Header />
-      
+
       <WelcomeSection />
       <Menu />
       <AboutUs />
-      <Testimonial/>
+      <Testimonial />
       <ContactUs />
       <Footer />
-      
     </div>
   );
-}
+};
 
 export default App;
